f400a0a66a4e56e6e3ecf51690368940cda26067
Mike Gostomski <mjg@pdx.edu>
2019-10-28 16:59:47 -0700

    Use 0.5.2

--- project_name/sass_variables.py
+++ project_name/sass_variables.py
@@ -5,6 +5,8 @@
 
 # ---- COLORS ----
 # Colors may be defined as their HTML color name, hex code, or as RGB or RGBA tuples
+# Color names may contain non-word characters for readability, and are not case-sensitive
+#     Example: "Light_Slate-Gray" would be converted to "lightslategray"
 PRIMARY_BG_COLOR = None
 PRIMARY_FG_COLOR = None
 HEADING_COLOR = None
@@ -13,19 +15,19 @@ CODE_COLOR = None
 
 # By default, primary and default (i.e. btn-primary, btn-default) colors are based on primary bg and fg colors above
 # Depending on what you selected, the default variations of them may look awful!
-BOOTSTRAP_PRIMARY = None                # psugreen
+BOOTSTRAP_PRIMARY = None                # psu-green
 BOOTSTRAP_PRIMARY_COMPLIMENT = None     # white
-BOOTSTRAP_DEFAULT = None                # psugray
+BOOTSTRAP_DEFAULT = None                # psu-gray
 BOOTSTRAP_DEFAULT_COMPLIMENT = None     # white
 
 # Standard danger, warning, etc colors
-BOOTSTRAP_DANGER = None                 # psusienna (red)
+BOOTSTRAP_DANGER = None                 # psu-red
 BOOTSTRAP_DANGER_COMPLIMENT = None      # white
-BOOTSTRAP_WARNING = None                # psuorange (yellowish)
-BOOTSTRAP_WARNING_COMPLIMENT = None     # psubrown
-BOOTSTRAP_INFO = None                   # psublue
+BOOTSTRAP_WARNING = None                # psu-orange (yellowish)
+BOOTSTRAP_WARNING_COMPLIMENT = None     # psu-brown
+BOOTSTRAP_INFO = None                   # psu-blue
 BOOTSTRAP_INFO_COMPLIMENT = None        # white
-BOOTSTRAP_SUCCESS = None                # psuaccentgreen
+BOOTSTRAP_SUCCESS = None                # psu-accent-green
 BOOTSTRAP_SUCCESS_COMPLIMENT = None     # white
 
 # ---- FONTS ----
