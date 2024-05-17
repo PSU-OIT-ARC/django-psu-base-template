c825a7550bf8eb17a2c8f51c512d66f7be70bf57
Mike Gostomski <mjg@pdx.edu>
2024-05-17 11:27:23 -0400

    Updated platform/hooks/prebuild/packages to work with Graviton or x86 processor

--- dot-platform/hooks/prebuild/01_packages.sh
+++ dot-platform/hooks/prebuild/01_packages.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 yum -y install libmemcached-devel
-yum -y install zlib-devel.x86_64
+
+lscpu | grep Architecture | grep x86
+
+if [ $? -eq 0 ]; then
+  echo "Has x86 processor"
+  yum -y install zlib-devel.x86_64
+else
+  echo "Has Graviton processor"
+  yum -y install zlib-devel
+  yum -y install gcc-c++
+fi
